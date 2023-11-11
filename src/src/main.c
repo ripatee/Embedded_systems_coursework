0d3f6b7df847bdb2e2a178aed1fd443937b77d74
Risto Korhonen <rikorhon19@student.oulu.fi>
2023-11-11 15:12:33 +0200

    Add OLED display (main-loop update not working yet)

--- src/src/main.c
+++ src/src/main.c
@@ -6,6 +6,9 @@
 #include <zephyr/net/socket.h>
 #include <zephyr/net/mqtt.h>
 
+#include <lvgl.h>
+#include <zephyr/drivers/display.h>
+
 #include "sensors.h"
 #include "buttons.h"
 #include "lte.h"
@@ -27,12 +30,43 @@ int main(void)
     char mqtt_msg[256];
     int alert_sent = 0;
 
+    const struct device *display_dev;
+    lv_obj_t *hello_world_label;
+    lv_obj_t *counter_label;
+    
+    char count_str[11] = {0};
+    uint32_t count = 0;
 
     LOG_INF("Application starting");
 
     /* Initialising everything */
     sensors_init();
     buttons_init();
+
+    display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+    if (!device_is_ready(display_dev))
+    {
+        LOG_ERR("Display not ready");
+    }
+
+    // Set screen colors, as by default they were inverted and the included version 
+    // of SSD1306 driver doesn't have inversion-on property yet
+    lv_obj_set_style_bg_color(lv_scr_act(), lv_color_black(), LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(lv_scr_act(), 255, LV_PART_MAIN);
+    lv_obj_set_style_text_color(lv_scr_act(), lv_color_white(), LV_PART_MAIN);
+
+    hello_world_label = lv_label_create(lv_scr_act());
+    counter_label = lv_label_create(lv_scr_act());
+
+
+    lv_label_set_text(hello_world_label, "Hello world!");
+    lv_label_set_text(counter_label, "");
+
+    lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_align(counter_label, LV_ALIGN_CENTER, 0, 16);
+
+    lv_task_handler();
+    display_blanking_off(display_dev);
 
     if (lte_modem_configure())
     {
@@ -130,6 +164,11 @@ do_connect:
             LOG_ERR("Failed to send MQTT message");
         }
 
+        sprintf(count_str, "%d", count);
+        lv_label_set_text(counter_label, count_str);
+        lv_task_handler();
+        count++;
+
         k_msleep(SLEEP_TIME_MS);
 
     }
@@ -141,4 +180,6 @@ do_connect:
         LOG_ERR("Could not disconnect MQTT client: %d", err);
     }
     goto do_connect;
+
+    return 0;
 }
